6462b27039d38abe0765ed94e83aaa071a8e3bd4
spartanGhost17 <adambaku5216@gmail.com>
2023-02-18 19:30:08 +0000

    corrected back-end to gradle to maven

---
+++ youtube-clone-back-end/src/main/java/com/project/youtube/youtubeclonebackend/YoutubeCloneBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.project.youtube.youtubeclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeCloneBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeCloneBackEndApplication.class, args);
+	}
+
+}
---
+++ youtube-clone-back-end/src/test/java/com/project/youtube/youtubeclonebackend/YoutubeCloneBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.youtube.youtubeclonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
