33e2752b694ce9d964353b0f7c286ffa6485b261
spartanGhost17 <adambaku5216@gmail.com>
2023-03-04 23:57:15 +0000

    update userRepository to <T, ID> instead of <ID, T>

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/repository/UserRepository.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.project.youtube.repository;
 import com.project.youtube.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<String, User> {
+public interface UserRepository extends MongoRepository<User, String> {
 }
