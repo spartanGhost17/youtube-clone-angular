e5a1d8dd2c076946b689c899cd5d324c76d586e4
spartanGhost17 <adambaku5216@gmail.com>
2023-05-08 15:05:24 +0100

    updated show modal behavior

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/modal/modal.component.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, Input, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, ComponentRef, ElementRef, EventEmitter, HostListener, Input, Output, SimpleChanges, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -12,36 +12,74 @@ export class ModalComponent {
   @Input() headerTemplate!: TemplateRef<any>;
   @Input() bodyTemplate!: TemplateRef<any>;
   @Input() footerTemplate: TemplateRef<any>;
-  @Input() show: boolean = false;
+  @Input() show: boolean;
+  @Input() modalWidth: string = '85%';
+  @Input() modalHeight: string = '100%';
+  @Input() color: string = 'transparent';
+  @Input() zIndex: string = '1';
+  @Input() top: string = '0';
+  @Input() left: string = '200px';
+
+  @Output() isShowUpdateEvent: EventEmitter<boolean> = new EventEmitter();
 
   constructor() {
 
   }
 
+  ngOnInit() {
+    if(this.show) {
+      //this.modal!.nativeElement.style.display = 'block';
+    }
+  }
   /**
-   * if scrubbing on continue scrubbing outside timeline container
-   * @param event mouseup event
-   */
-  @HostListener('document:click', ['$event']) 
-  handleClickEvent(event: MouseEvent){
-    //if(this.show) {
-    //  console.log("====> clicked body hide modal =====");
-      //this.show = false;
-    //}
+   * on click event on document
+   * @param event 
+  */
+  //@HostListener('document:click', ['$event'])
+  //onClick(event: MouseEvent) {
+  //  console.log('click on document ', this.show);
+    //if clicked element is not inside modal
+  //  if (this.show && !this.modal.nativeElement.contains(event.target)) {
+  //    console.log('ddddisplay');
+  //    this.modal.nativeElement.style.display = 'none';
+  //  }
+  //}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.show.currentValue) {
+      console.log('inside ngOnChanges modal [TRUE] ', changes.show.currentValue);
+      if(this.modal){
+        this.modal.nativeElement.style.display = 'block';
+      }
+    }
+    else {
+      console.log('inside ngOnChanges modal [FALSE] ',changes.show.currentValue);
+      this.modal.nativeElement.style.display = 'none';
+    }
   }
 
   ngAfterViewInit() {
-    //this.header.createEmbeddedView(this.headerTemplate);
+    console.log("after viewinit modal ", this.show);
+    this.showModal(this.show);
+  }
+
+  showModal(show: boolean) {
+    if(show) {
+      console.log("after view inside if modal", show);
+      this.modal.nativeElement.style.display = 'block';
+    }
+    else {
+      this.modal.nativeElement.style.display = 'none';
+    }
   }
 
   ngOnDestroy() : void {
-
-  }
-
-  destroy() : void {
+    console.log("component being destroyed ")
   }
 
   closeModal(): void {
+
+    this.isShowUpdateEvent.emit(!this.show);
     this.modal.nativeElement.style.display = 'none';
   }
 }
