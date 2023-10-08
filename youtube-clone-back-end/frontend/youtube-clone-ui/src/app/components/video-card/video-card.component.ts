fb79782c3b920cbd07baec3c03087fda8fe588d2
spartanGhost17 <adambaku5216@gmail.com>
2023-10-08 22:51:07 +0100

    updated video card

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/video-card/video-card.component.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/video-card/video-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-video-card',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./video-card.component.scss']
 })
 export class VideoCardComponent {
+  @Input() title:string = 'Title saaaaaaa aaaaaa aaaaaaaaaaaaaaaaa aeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee azzdsassad dffffffffffffffffff';
+  @Input() channelName:string = 'Channel name';
+  @Input() likeCount:string = '12.5K';
+  @Input() postTime:string = '4 hours ago';
 
+  @Input() ThumbnailURL:string = '../../../assets/grand_tourismo.jpg';
+  @Input() videoURL:string = '../../../assets/test-videos/Y2Mate.is - Gran Turismo 5 Opening Montage-6Z1TL_VEEQo-720p-1654232433855.mp4';
+  @Input() style: string = 'default';//either 'default' or 'horizontal'
+  constructor() {}
 }
