2d4a6149e8df0b58ea2c03e6b23dec9a7ca3298c
spartanGhost17 <adambaku5216@gmail.com>
2024-02-06 17:10:23 +0000

    added comments holder component

---
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/comments-holder/comments-holder.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommentsHolderComponent } from './comments-holder.component';
+
+describe('CommentsHolderComponent', () => {
+  let component: CommentsHolderComponent;
+  let fixture: ComponentFixture<CommentsHolderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CommentsHolderComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CommentsHolderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/comments-holder/comments-holder.component.ts
@@ -0,0 +1,62 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Comment } from '../../models/comment';
+@Component({
+  selector: 'app-comments-holder',
+  templateUrl: './comments-holder.component.html',
+  styleUrls: ['./comments-holder.component.scss']
+})
+export class CommentsHolderComponent implements OnInit {
+  @Input() comments: Comment[];
+  //comments_copy: Comment[];
+  @Input() user: any = {
+    userId: 'timbaWolf',
+    iconURL: '../../../assets/batman_red_glow.jpg'
+  };
+  commentInput: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onCancelReply(): void {
+    this.commentInput = '';
+  }
+
+  onReply(): void {
+    let commentsCount = this.comments.length+1;
+    let comment: Comment = {
+      id: commentsCount.toString(), //ID will come from the db auto incrementing
+      commentText: this.commentInput,
+      imageURL: this.user.iconURL,
+      userId: this.user.userId,
+      likeCount: 0,
+      dislikeCount: 0,
+      postTime: '2 hours',
+      replyCount: 0,   
+      subComments: []
+    }
+    this.commentInput = '';
+    this.comments = [...this.comments, comment];
+    //this.comments_copy = JSON.parse(JSON. stringify(this.comments))
+  }
+
+  onCommentUpdate(updatedComment: any, originalComment: Comment): void {
+
+    console.log("comment updated", updatedComment);
+    console.log("old comment", originalComment);
+
+    //console.log("CLONE ", this.comments_copy)
+    console.log("whole comment list before ", this.comments);
+
+    // Find the index of the originalComment in the comments array
+    const index = this.comments.indexOf(updatedComment)//(originalComment);
+    console.log("index", index);
+    // Update the comments array with the updatedComment at the found index
+    if (index !== -1) {
+      this.comments[index] = updatedComment;
+    }
+
+    console.log("whole comment list after ", this.comments);
+  }
+
+}
