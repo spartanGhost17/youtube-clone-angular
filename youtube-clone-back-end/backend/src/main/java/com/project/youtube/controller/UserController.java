e07b753cee948f5380bc9febedffdeffeb028cac
spartanGhost17 <adambaku5216@gmail.com>
2024-02-14 21:53:40 +0000

    update user controller

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/controller/UserController.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/controller/UserController.java
@@ -1,10 +1,24 @@
 package com.project.youtube.controller;
 
-import com.project.youtube.dto.UserDto;
+import com.project.youtube.dto.UserDTO;
+import com.project.youtube.model.HttpResponse;
+import com.project.youtube.model.User;
 import com.project.youtube.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value="/api/v1/user/")
@@ -13,15 +27,31 @@ public class UserController {
     @Autowired
     private UserServiceImpl userServiceImpl;
 
-    @PostMapping(value="signup")
-    public UserDto createNewUser(@RequestBody UserDto userDto) {
-        return userServiceImpl.createUser(userDto);
+    @PostMapping(value="register")
+    public ResponseEntity<HttpResponse> createNewUser(@RequestBody @Valid User user) {
+        UserDTO userDTO = userServiceImpl.createUser(user);
+        return ResponseEntity.created(getUri()).body(
+                HttpResponse.builder()
+                        .timeStamp(Instant.now().toString())
+                        .data(Map.of("user", userDTO))
+                        .message("user created")
+                        .status(HttpStatus.CREATED)
+                        .statusCode(HttpStatus.CREATED.value())
+                        .build()
+        );
     }
 
-    //@GetMapping(value="login")
-    @PutMapping(value="playlist")
-    public UserDto updatePlaylist() {
-        return null;
+    private URI getUri() {
+        return URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/get/<userId>").toUriString());
     }
+    /*public UserDTO createNewUser(@RequestBody UserDTO userDTO) {
+        return userServiceImpl.createUser(UserDTOMapper.toUser(userDTO));
+    }*/
+
+    //@GetMapping(value="login")
+    /*@PutMapping(value="playlist")
+    public UserDTO updatePlaylist() {
+        return null;
+    }*/
 
 }
