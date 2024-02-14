0e6c4ac6e1b8bb23d47daa0ac9a81ecf94c32e3a
spartanGhost17 <adambaku5216@gmail.com>
2024-02-14 21:43:32 +0000

    updated security config with bcrypt bean

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/config/SecurityConfig.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/config/SecurityConfig.java
@@ -1,6 +1,18 @@
 package com.project.youtube.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig  {
+
+    private static final int STRENGTH = 12; //can pass this to BCRYPT
+    @Bean(value = "passwordEncoder")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
