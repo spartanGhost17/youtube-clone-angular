69b3a203993f3607f157bf0e8b0503ec4ed60250
spartanGhost17 <adambaku5216@gmail.com>
2024-02-14 21:47:49 +0000

    update user interface

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/UserService.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/UserService.java
@@ -1,13 +1,9 @@
 package com.project.youtube.service;
 
-import com.project.youtube.dto.UserDto;
+import com.project.youtube.dto.UserDTO;
 import com.project.youtube.model.User;
 
 
 public interface UserService {
-    UserDto createUser(UserDto userDto);
-
-    UserDto signIn(UserDto userDto);
-
-    User getUserById(String userId);
+    UserDTO createUser(User user);
 }
