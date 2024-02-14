122fa099d9c8ce2f8182e81f679a24adbab827d1
spartanGhost17 <adambaku5216@gmail.com>
2024-02-14 21:36:48 +0000

    exclude security

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/YoutubeCloneApplication.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/YoutubeCloneApplication.java
@@ -2,8 +2,9 @@ package com.project.youtube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication//(scanBasePackages = "com.project.youtube.backend")
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })//(scanBasePackages = "com.project.youtube.backend")
 public class YoutubeCloneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YoutubeCloneApplication.class, args);
