6dda58c69282fbfe014326b091079f9704a451f1
spartanGhost17 <adambaku5216@gmail.com>
2024-02-14 22:00:11 +0000

    updated UserDTO

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/dto/UserDto.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/dto/UserDto.java
@@ -4,23 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-import java.util.Set;
+import java.sql.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDto {
-    private String id;
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String channelName;
+    private Date createdAt;
+    private String email;
+    private Boolean verified;
+    private String description;
+    private Boolean enabled;
+    private Boolean isNotLocked;
+    private Boolean isUsingMFA;
+    private String profilePicture;
+    /*private String id;
     private String firstName;
     private String lastName;
     private String userName;
-    private String emailAddress;
-    private String profilePictureURL;
+    private String emailAddress;*/
+
+    /*
+    //playlist and other info
     private Set<String> subscribedToUsers;
     private Set<String> subscribers;
     private List<String> videoHistory;
     private Set<String> likedVideos;
     private Set<String> dislikedVideos;
-    private List<String> PlayListsIds;
+    private List<String> PlayListsIds;*/
 }
