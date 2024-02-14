808e139818b7b81fe6b02ffb1e23ed8bda50b0f7
spartanGhost17 <adambaku5216@gmail.com>
2024-02-14 21:51:39 +0000

    updated implemented createUser

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/impl/UserServiceImpl.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/impl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.project.youtube.service.impl;
 
-import com.project.youtube.dto.UserDto;
+import com.project.youtube.dao.UserDao;
+import com.project.youtube.dao.impl.UserDaoImpl;
+import com.project.youtube.dto.UserDTO;
+import com.project.youtube.dtomapper.UserDTOMapper;
 import com.project.youtube.model.User;
-import com.project.youtube.repository.UserRepository;
 import com.project.youtube.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -11,47 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Set;
-
 @Service
 @RequiredArgsConstructor
-@Component
 public class UserServiceImpl implements UserService {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
 
+    @Autowired
+    private UserDaoImpl userDaoImpl;
     //@Autowired
     //private UserRepository userRepository;
 
     @Override
-    public UserDto createUser(UserDto userDto) {
-        User user = new User();
-        user.setFirstName(userDto.getFirstName());
-        user.setLastName(userDto.getLastName());
-        user.setUserName(userDto.getUserName());
-        user.setEmailAddress(userDto.getEmailAddress());
-
-        //var savedUser = userRepository.save(user);
-        var savedUser = new User();
-        return new UserDto(savedUser.getId(), user.getFirstName(), user.getLastName(), user.getUserName(),null, null, null, null, null, null, null, null);
+    public UserDTO createUser(User user) {
+        User createdUser = userDaoImpl.create(user);
+        return UserDTOMapper.toUserDTO(createdUser);
     }
-
-    @Override
-    public UserDto signIn(UserDto userDto) {
-        return null;
-    }
-
-    @Override
-    public User getUserById(String UserId) {
-        LOGGER.info("Entering getVideoById");
-        LOGGER.info("Getting video for video id : "+UserId);
-        return null;/*userRepository.findById(UserId)
-                .orElseThrow(() -> {
-                    LOGGER.error("Cannot retreive video by id:" + UserId);
-                    return new NoSuchElementException("Cannot retreive video by id: " + UserId);
-                });*/
-    }
-
 }
