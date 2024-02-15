2983c428036e919e59ca07ba3f50c60fc4249067
spartanGhost17 <adambaku5216@gmail.com>
2024-02-15 15:11:08 +0000

    updated role and user queries

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/query/RoleQuery.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/query/RoleQuery.java
@@ -3,4 +3,6 @@ package com.project.youtube.query;
 public class RoleQuery {
     public static final String SELECT_ROLE_BY_NAME_QUERY = "SELECT * FROM Roles WHERE name = :name";
     public static final String INSERT_ROLE_TO_USER_QUERY = "INSERT INTO UserRoles(user_id, role_id) VALUES(:userId, :roleId)";
+    public static final String SELECT_ROLE_BY_USERNAME_QUERY = "SELECT ur.id, r.name, r.permissions, u.username FROM Roles r INNER JOIN UserRoles ur ON ur.role_id = r.id INNER JOIN Users u ON u.id = ur.user_id WHERE u.username = :username";
+    public static final String SELECT_ROLE_BY_EMAIL_QUERY = "SELECT ur.id, r.name, r.permissions, u.email FROM Roles r INNER JOIN UserRoles ur ON ur.role_id = r.id INNER JOIN Users u ON u.id = ur.user_id WHERE u.email = :email";
 }
--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/query/UserQuery.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/query/UserQuery.java
@@ -4,4 +4,6 @@ public class UserQuery {
     public static final String COUNT_USER_EMAIL_QUERY = "SELECT COUNT(*) FROM Users WHERE email = :email";
     public static final String INSERT_USER_QUERY = "INSERT INTO Users(username, channel_name, email, password) VALUES(:username, :channelName, :email, :password)";
     public static final String INSERT_ACCOUNT_VERIFICATION_URL_QUERY = "INSERT INTO AccountVerifications(user_id, url) VALUES(:userId, :url)";
+    public static final String SELECT_USER_BY_EMAIL_QUERY = "SELECT * FROM Users WHERE email = :email";
+    public static final String SELECT_USER_BY_USERNAME_QUERY = "SELECT * FROM Users WHERE username = :username";
 }
