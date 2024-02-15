8f3539cbcc974366387f25c557ba65730cfe1262
spartanGhost17 <adambaku5216@gmail.com>
2024-02-15 15:04:13 +0000

    created CustomAccessDeniedHandler

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,37 @@
+package com.project.youtube.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.youtube.model.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.Instant;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        HttpResponse httpResponse = HttpResponse.builder()
+                .timeStamp(Instant.now().toString())
+                .reason("You don't have enough permission to access this resource")
+                .status(HttpStatus.FORBIDDEN)
+                .statusCode(HttpStatus.FORBIDDEN.value())
+                .build();
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);//converts(serializes) httpResponse to JSON and write it to the output stream response
+        outputStream.flush();
+        outputStream.close();//necessary?
+    }
+}
