7eff6158e62161adfac183faa8781dad834eb369
spartanGhost17 <adambaku5216@gmail.com>
2024-02-15 21:24:44 +0000

    Added user login form

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/form/LoginForm.java
@@ -0,0 +1,13 @@
+package com.project.youtube.form;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class LoginForm {
+    @NotEmpty(message = "username cannot be empty")
+    private String username;
+    @NotEmpty(message = "password cannot be empty")
+    private String password;
+}
