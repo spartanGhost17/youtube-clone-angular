c78dcf4e5ed1627914951643bdaedf1c950e1cb1
spartanGhost17 <adambaku5216@gmail.com>
2024-02-15 15:08:00 +0000

    updated role service

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/RoleService.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/RoleService.java
@@ -1,5 +1,16 @@
 package com.project.youtube.service;
 
+import com.project.youtube.model.Role;
+
+import java.util.List;
+import java.util.Set;
+
 public interface RoleService {
     void adduserToRole(Long userId, String roleName);
+    Set<Role> getRoleByUserId(Long username);
+
+    Set<Role> getRoleByUsername(String username);
+
+    Set<Role> getRoleByUserEmail(String email);
+    void updateUserRole(Long userId, String roleName);
 }
--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/impl/RoleServiceImpl.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/impl/RoleServiceImpl.java
@@ -1,22 +1,40 @@
 package com.project.youtube.service.impl;
 
-import com.project.youtube.dao.RoleDao;
 import com.project.youtube.dao.impl.RoleDaoImpl;
+import com.project.youtube.model.Role;
 import com.project.youtube.service.RoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
-@Component //is it necessary?
 public class RoleServiceImpl implements RoleService {
     @Autowired
     private final RoleDaoImpl roleDaoImpl;//should be impl
-
     @Override
     public void adduserToRole(Long userId, String roleName) {
         roleDaoImpl.addRoleToUser(userId, roleName);
+    }
+
+    @Override
+    public Set<Role> getRoleByUserId(Long username) {
+        return null;
+    }
+
+    @Override
+    public Set<Role> getRoleByUsername(String username) {
+        return roleDaoImpl.getRoleByUsername(username);
+    }
+    @Override
+    public Set<Role> getRoleByUserEmail(String email) {
+        return roleDaoImpl.getRoleByUserEmail(email);
+    }
+    @Override
+    public void updateUserRole(Long userId, String roleName) {
+
     }
 }
