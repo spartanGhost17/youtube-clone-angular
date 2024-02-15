d4162aeb6b1b79630c9617862aada88001e856f4
spartanGhost17 <adambaku5216@gmail.com>
2024-02-15 15:05:03 +0000

    created CustomAuthenticationEntryPoint

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/handler/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+package com.project.youtube.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.youtube.model.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.Instant;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        HttpResponse httpResponse = HttpResponse.builder()
+                .timeStamp(Instant.now().toString())
+                .reason("You don't have enough permission to access this resource")
+                .status(HttpStatus.UNAUTHORIZED)
+                .statusCode(HttpStatus.UNAUTHORIZED.value())
+                .build();
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);//converts(serializes) httpResponse to JSON and write it to the output stream response
+        outputStream.flush();
+        outputStream.close();//necessary?
+    }
+}
