d178f1ce476702935df768927eeafb22d7f5c643
spartanGhost17 <adambaku5216@gmail.com>
2024-02-15 15:11:41 +0000

    create JWT security constants

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/constants/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.project.youtube.constants;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class SecurityConstants {
+    public static final String JWT_KEY = "";
+    public static final String JWT_HEADER = "Authorization";
+}
