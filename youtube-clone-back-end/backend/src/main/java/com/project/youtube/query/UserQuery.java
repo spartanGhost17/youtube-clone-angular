39af1ea260c3f7533f054b129fef3566541e1198
spartanGhost17 <adambaku5216@gmail.com>
2024-02-16 00:14:02 +0000

    added two factor verification queries

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/query/UserQuery.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/query/UserQuery.java
@@ -6,4 +6,6 @@ public class UserQuery {
     public static final String INSERT_ACCOUNT_VERIFICATION_URL_QUERY = "INSERT INTO AccountVerifications(user_id, url) VALUES(:userId, :url)";
     public static final String SELECT_USER_BY_EMAIL_QUERY = "SELECT * FROM Users WHERE email = :email";
     public static final String SELECT_USER_BY_USERNAME_QUERY = "SELECT * FROM Users WHERE username = :username";
+    public static final String DELETE_VERIFICATION_CODE_BY_USER_ID_QUERY = "DELETE FROM TwoFactorVerifications WHERE user_id = :userId";
+    public static final String INSERT_VERIFICATION_CODE_QUERY = "INSERT INTO TwoFactorVerifications (user_id, code, expiration_date) VALUES (:userId, :code, :expirationDate)";
 }
