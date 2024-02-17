bd51821e19d14baa333626b261baa8e83fb0965c
spartanGhost17 <adambaku5216@gmail.com>
2024-02-17 22:16:58 +0000

    added overload toUserDTO

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/dtomapper/UserDTOMapper.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/dtomapper/UserDTOMapper.java
@@ -1,9 +1,13 @@
 package com.project.youtube.dtomapper;
 
 import com.project.youtube.dto.UserDTO;
+import com.project.youtube.model.Role;
 import com.project.youtube.model.User;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Component
 public class UserDTOMapper {
@@ -13,10 +17,18 @@ public class UserDTOMapper {
         return userDTO;
     }
 
+    public static UserDTO toUserDTO(User user, Role role) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setAuthorities(new ArrayList<>(Arrays.asList(role)));
+        BeanUtils.copyProperties(user, userDTO);//copies fields
+        return userDTO;
+    }
+
     public static User toUser(UserDTO userDTO) {
         User user = new User();
         BeanUtils.copyProperties(userDTO, user);
         return user;
     }
+
 }
 
