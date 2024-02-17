d298929d621415b032ef01240a9d08af8d0cf412
spartanGhost17 <adambaku5216@gmail.com>
2024-02-17 23:11:35 +0000

    deleted unused imports

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/handler/APIExceptionHandler.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/handler/APIExceptionHandler.java
@@ -6,13 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.servlet.http.HttpServletRequest;
-import java.net.URI;
 import java.time.Instant;
 
 @ControllerAdvice
