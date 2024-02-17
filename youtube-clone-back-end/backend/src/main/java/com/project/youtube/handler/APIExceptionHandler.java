02653917f9d722a6cd5d45f398b640fbcaa4aa19
spartanGhost17 <adambaku5216@gmail.com>
2024-02-17 23:05:35 +0000

    updated APIExceptionHandler

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/handler/APIExceptionHandler.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/handler/APIExceptionHandler.java
@@ -6,22 +6,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
 import java.time.Instant;
 
 @ControllerAdvice
 public class APIExceptionHandler {
     @ExceptionHandler (APIException.class)
     public ResponseEntity<HttpResponse> handleAPIException(APIException exception) {
-        HttpResponse httpResponse = HttpResponse.builder()
-                .timeStamp(Instant.now().toString())
-                .reason("Something went bad")
-                .message(exception.getMessage())
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .build();
         return ResponseEntity
                 .internalServerError()
-                .body(httpResponse);
+                .body(HttpResponse.builder()
+                        .timeStamp(Instant.now().toString())
+                        .reason("Something went bad")
+                        .message(exception.getMessage())
+                        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                        .build());
     }
 }
