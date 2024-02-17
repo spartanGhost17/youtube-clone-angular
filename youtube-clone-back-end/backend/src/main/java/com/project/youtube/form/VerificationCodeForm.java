3b3a31b3596ca57432d59d2a755a9a97d3169774
spartanGhost17 <adambaku5216@gmail.com>
2024-02-17 19:06:33 +0000

    updated verification code form

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/form/VerificationCodeForm.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/form/VerificationCodeForm.java
@@ -1,2 +1,13 @@
-package com.project.youtube.form;public class VerificationCodeForm {
+package com.project.youtube.form;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class VerificationCodeForm {
+    @NotEmpty(message = "username cannot be empty")
+    private String username;
+    @NotEmpty(message = "code cannot be empty")
+    private String code;
 }
