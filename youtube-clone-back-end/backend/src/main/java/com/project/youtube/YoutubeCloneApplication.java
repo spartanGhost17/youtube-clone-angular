7f8f498b429976e83ada28279d1b193407621f1e
spartanGhost17 <adambaku5216@gmail.com>
2024-02-18 18:53:13 +0000

    commented out excluded fields

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/YoutubeCloneApplication.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/YoutubeCloneApplication.java
@@ -2,9 +2,10 @@ package com.project.youtube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+//import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })//(scanBasePackages = "com.project.youtube.backend")
+@SpringBootApplication //(exclude = { ErrorMvcAutoConfiguration.class } )//(exclude = { SecurityAutoConfiguration.class })//(scanBasePackages = "com.project.youtube.backend")
 public class YoutubeCloneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YoutubeCloneApplication.class, args);
