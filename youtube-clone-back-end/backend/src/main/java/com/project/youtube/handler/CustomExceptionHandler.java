a87754d8f24972cdcebc83e30aa786b43bc7b464
spartanGhost17 <adambaku5216@gmail.com>
2024-02-18 15:39:07 +0000

    created ecxception handler and moved ExceptionAPI to custom handler

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/handler/CustomExceptionHandler.java
@@ -0,0 +1,296 @@
+package com.project.youtube.handler;
+
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.project.youtube.Exception.APIException;
+import com.project.youtube.model.HttpResponse;
+import com.twilio.exception.ApiException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.time.LocalTime.now;
+import static org.springframework.http.HttpStatus.*;
+
+@RestControllerAdvice
+@Slf4j
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler implements ErrorController {
+    //ErrorController will help handle white label error 404
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception exception, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .path(request.getContextPath())
+                        .timeStamp(Instant.now().toString())
+                        .reason("An Internal Server Error Occurred")
+                        .developerMessage(exception.getMessage())//shouldn't pass this to front-end
+                        .status(status)
+                        .statusCode(status.value())
+                        .build(), status);
+
+    }
+
+    /**
+     * handle the javax (jakarta) validation errors
+     * @param exception
+     * @param headers
+     * @param status
+     * @param request
+     * @return response object with error fields
+     */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fieldErrorMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .path(request.getContextPath())
+                        .timeStamp(Instant.now().toString())
+                        .reason(fieldErrorMessage)
+                        .developerMessage(exception.getMessage())//shouldn't pass this to front-end in prod
+                        .status(status)
+                        .statusCode(status.value())
+                        .build(), status);
+    }
+
+    /**
+     * Thrown if constraint on the table is violated
+     * @param exception
+     * @return the response entity
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<HttpResponse> sQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException exception) {
+        log.error(exception.getMessage());
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason(exception.getMessage().contains("Duplicate entry") ? "Information already exists" : exception.getMessage())
+                        .developerMessage(exception.getMessage())
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value())
+                        .build(), BAD_REQUEST);
+    }
+
+    /**
+     * Handle spring security incorrect credentials provided to Authentication provider for creation of Authentication object
+     * @param exception the exception
+     * @return the response entity
+     */
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<HttpResponse> badCredentialsException(BadCredentialsException exception) {
+        log.error(exception.getMessage());
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason(exception.getMessage() + ", Incorrect email or password")
+                        .developerMessage(exception.getMessage())
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value())
+                        .build(), BAD_REQUEST);
+    }
+
+    /**
+     * handle response for API Exception entity
+     * @param exception the exception
+     * @return the response entity
+     */
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<HttpResponse> apiException(APIException exception) {
+        log.error(exception.getMessage());
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason(exception.getMessage())
+                        .developerMessage(exception.getMessage())
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value())
+                        .build(), BAD_REQUEST);
+    }
+
+    /**
+     * Access denied exception for non accessible resources to principal
+     * @param exception the exception
+     * @return the response entity
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<HttpResponse> accessDeniedException(AccessDeniedException exception) {
+        log.error(exception.getMessage());
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason("Access denied. You don\'t have access")
+                        .developerMessage(exception.getMessage())
+                        .status(FORBIDDEN)
+                        .statusCode(FORBIDDEN.value())
+                        .build(), FORBIDDEN);
+    }
+
+    /**
+     * Handle record not found or generic error
+     * @param exception the exception
+     * @return the response entity
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HttpResponse> exception(Exception exception) {
+        log.error(exception.getMessage());
+        System.out.println(exception);
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason(exception.getMessage() != null ?
+                                (exception.getMessage().contains("expected 1, actual 0") ? "Record not found" : exception.getMessage())
+                                : "Some error occurred")
+                        .developerMessage(exception.getMessage())
+                        .status(INTERNAL_SERVER_ERROR)
+                        .statusCode(INTERNAL_SERVER_ERROR.value())
+                        .build(), INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Error in decoding the jwt token (either get subject)
+     * @param exception the exception
+     * @return the response
+     */
+    @ExceptionHandler(JWTDecodeException.class)
+    public ResponseEntity<HttpResponse> exception(JWTDecodeException exception) {
+        log.error(exception.getMessage());
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason("Could not decode the token")
+                        .developerMessage(exception.getMessage())
+                        .status(INTERNAL_SERVER_ERROR)
+                        .statusCode(INTERNAL_SERVER_ERROR.value())
+                        .build(), INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * thrown when an empty result set
+     * @param exception exception
+     * @return the response
+     */
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<HttpResponse> emptyResultDataAccessException(EmptyResultDataAccessException exception) {
+        log.error(exception.getMessage());
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason(exception.getMessage().contains("expected 1, actual 0") ? "Record not found" : exception.getMessage())
+                        .developerMessage(exception.getMessage())
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value())
+                        .build(), BAD_REQUEST);
+    }
+
+    /**
+     * Disabled user principal exception
+     * @param exception the exception
+     * @return the response
+     */
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<HttpResponse> disabledException(DisabledException exception) {
+        log.error(exception.getMessage());
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .developerMessage(exception.getMessage())
+                        //.reason(exception.getMessage() + ". Please check your email and verify your account.")
+                        .reason("User account is currently disabled")
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value()).build()
+                , BAD_REQUEST);
+    }
+
+    /**
+     * thrown if account is locked
+     * @param exception the exception
+     * @return the response
+     */
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<HttpResponse> lockedException(LockedException exception) {
+        log.error(exception.getMessage());
+        return   new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .developerMessage(exception.getMessage())
+                        //.reason(exception.getMessage() + ", too many failed attempts.")
+                        .reason("User account is currently locked")
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value()).build()
+                , BAD_REQUEST);
+    }
+
+    /**
+     * JDBC template query exception
+     * @param exception the exception
+     * @return the response
+     */
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<HttpResponse> dataAccessException(DataAccessException exception) {
+        log.error(exception.getMessage());
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason(processErrorMessage(exception.getMessage()))
+                        .developerMessage(processErrorMessage(exception.getMessage()))
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value()).build()
+                , BAD_REQUEST);
+    }
+
+    /**
+     * create http response
+     * @param httpStatus the status
+     * @param reason the reason
+     * @param exception the exception
+     * @return the response
+     */
+    private ResponseEntity<HttpResponse> createErrorHttpResponse(HttpStatus httpStatus, String reason, Exception exception) {
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .developerMessage(exception.getMessage())
+                        .reason(reason)
+                        .status(httpStatus)
+                        .statusCode(httpStatus.value()).build()
+                , httpStatus);
+    }
+
+    /**
+     * processor error message
+     * @param errorMessage the error message
+     * @return the response string
+     */
+    private String processErrorMessage(String errorMessage) {
+        if(errorMessage != null) {
+            if(errorMessage.contains("Duplicate entry") && errorMessage.contains("AccountVerifications")) {
+                return "You already verified your account.";
+            }
+            if(errorMessage.contains("Duplicate entry") && errorMessage.contains("ResetPasswordVerifications")) {
+                return "We already sent you an email to reset your password.";
+            }
+            if(errorMessage.contains("Duplicate entry")) {
+                return "Duplicate entry. Please try again.";
+            }
+        }
+        return "Some error occurred";
+    }
+}
