072d52dbcf3d015221d8fee222dc05ff830c8050
spartanGhost17 <adambaku5216@gmail.com>
2024-02-19 00:58:35 +0000

    whitelisted reset password path

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/config/SecurityConfig.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     private final CustomAuthenticationProvider customAuthenticationProvider;
     private final AuthorizationFilter authorizationFilter;
     private final NotFoundResourceController notFoundResourceController;
-    private static final String[] PUBLIC_URLS = { API_VERSION + "user/login/**", API_VERSION + "user/register/**", API_VERSION + "user/verify/code/**", "/error" };
+    private static final String[] PUBLIC_URLS = { API_VERSION + "user/login/**", API_VERSION + "user/register/**", API_VERSION + "user/verify/code/**", "/error", API_VERSION+"user/resetpassword/**" };
 
     /**
      * registers authentication providers with the authentication manager
