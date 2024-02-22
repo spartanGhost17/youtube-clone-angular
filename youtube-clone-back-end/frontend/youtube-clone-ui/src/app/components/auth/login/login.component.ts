abfe1a665502114791bdf4beede0462d8ebca471
spartanGhost17 <adambaku5216@gmail.com>
2024-02-22 23:44:36 +0000

    updated the login component

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/auth/login/login.component.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/auth/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  showPassword: boolean = false;
+  @ViewChild('passwordInput') passwordInput: ElementRef<any>;
 
+  /**
+   * check if submit conditions have been met before sending form
+   */
+  onSubmit() {
+    console.log(`${this.username} the user name `);
+    console.log(`${this.password} the password `);
+  }
+
+  /**
+   *  Toggle the input type between "password" and "text"
+   */
+  togglePasswordVisibility(input: any) {
+    this.showPassword = !this.showPassword;
+    input.type = input.type === 'password' ? 'text' : 'password';
+  }
 }
