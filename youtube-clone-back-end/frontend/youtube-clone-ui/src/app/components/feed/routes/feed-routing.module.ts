ee8fdd74b7eff7ff6f8f714fab9dd37b6c39cfd7
spartanGhost17 <adambaku5216@gmail.com>
2024-02-24 18:52:22 +0000

    formated background gradient directive

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/feed/routes/feed-routing.module.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/feed/routes/feed-routing.module.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { HistoryViewComponent } from 'src/app/components/feed/history-view/history-view.component';
+import { HistoryViewComponent } from '../../feed/history-view/history-view.component';
 import { PlaylistDashboardComponent } from '../playlist/playlist-dashboard/playlist-dashboard.component';
 import { SubscriptionsViewComponent } from '../subscriptions-view/subscriptions-view.component';
 console.warn('FEED MODE HAS BEEN LOADED');
--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/directives/background-gradiant/container-bg-gradiant.directive.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/directives/background-gradiant/container-bg-gradiant.directive.ts
@@ -1,9 +1,10 @@
 import { AfterViewInit, Directive, ElementRef, Input, SimpleChanges } from '@angular/core';
 //import ColorThief from 'colorthief';
-import Vibrant from 'node-vibrant';
+import Vibrant from 'node-vibrant';//stable version is @3.1.6
+
 @Directive({
-    selector: '[appContainerBgGradiant]',
-    standalone: true
+  selector: '[appContainerBgGradiant]',
+  standalone: true,
 })
 export class ContainerBgGradiantDirective implements AfterViewInit {
   @Input() thumbnailURL: string;
@@ -14,27 +15,24 @@ export class ContainerBgGradiantDirective implements AfterViewInit {
   }
 
   applyGradiant() {
-    let src = this.thumbnailURL;
-    Vibrant.from(src).getPalette((_err: any, palette: any) => {
-      if(palette) {
-        console.log("palette ", palette);
+    Vibrant.from(this.thumbnailURL).getPalette((_err: any, palette: any) => {
+      if (palette) {
+        console.log('palette ', palette);
         const primaryColor = palette.Vibrant!.hex;
         const secondaryColor = palette.Muted!.hex;
         const gradient = `linear-gradient(to bottom, ${primaryColor}, rgba(0, 0, 0, 0.800)), linear-gradient(to bottom, rgba(0, 0, 0, 1.000), ${secondaryColor})`;
         this.el.nativeElement.style.backgroundImage = gradient;
-        if ('backdropFilter' in document.documentElement.style) { //not all browsers may support backdrop filter
+        if ('backdropFilter' in document.documentElement.style) {
+          //not all browsers may support backdrop filter
           this.el.nativeElement.style.backdropFilter = 'blur(10px)';
         }
-
       }
-      
     });
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if(changes.thumbnailURL) {
+    if (changes.thumbnailURL) {
       this.applyGradiant();
     }
   }
-
 }
