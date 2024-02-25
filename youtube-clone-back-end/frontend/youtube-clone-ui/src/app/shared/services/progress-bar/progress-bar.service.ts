7e92648561c39c046dfce327d2239695457a36d5
spartanGhost17 <adambaku5216@gmail.com>
2024-02-25 22:05:12 +0000

    added ngx-progress bar service

---
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/services/progress-bar/progress-bar.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { NgProgressRef } from 'ngx-progressbar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProgressBarService {
+
+  progressRef: NgProgressRef;
+  defaultColor = '#FFFFFF';
+  successColor = '#FFFFF';
+  errorColor = '#EF0000';
+  currentColor = this.defaultColor;
+  constructor() { }
+  
+  startLoading() {
+    this.currentColor = this.defaultColor;
+    this.progressRef.start();
+  }
+
+  completeLoading() {
+    this.progressRef.complete();
+  }
+
+  setSuccessColor() {
+    this.currentColor = this.successColor;
+  }
+
+  setErrorColor() {
+    this.currentColor = this.errorColor;
+  }
+  /*changeProgressColor() {
+    this.progressRef.setConfig({ color: 'green' });
+  }*/
+}
