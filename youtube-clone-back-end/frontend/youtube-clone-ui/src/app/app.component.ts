671dbedf53d31551a11158a961f4376b498a0eb9
spartanGhost17 <adambaku5216@gmail.com>
2024-02-26 17:31:11 +0000

    moved loading bar to root app html

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/app.component.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/app.component.ts
@@ -2,24 +2,35 @@ import { Component, enableProdMode } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
 import { ComponentUpdatesService } from './shared/services/app-updates/component-updates.service';
+import { ProgressBarService } from './shared/services/progress-bar/progress-bar.service';
+import { NgProgress, NgProgressModule } from 'ngx-progressbar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgProgressModule],
 })
 export class AppComponent {
   isCollapsed = false;
   sidebarType: string = 'side'; //'side'
 
-  constructor(private componentUpdatesService: ComponentUpdatesService) {
+  constructor(
+    private componentUpdatesService: ComponentUpdatesService,
+    private progressBar: NgProgress,
+    public progressBarService: ProgressBarService
+  ) {
+
     console.log('sending side bar type ', this.sidebarType);
     this.componentUpdatesService.sideBarTypeUpdate(this.sidebarType);
     if (environment.production) {
       enableProdMode();
     }
     console.log(`CURRENT API URL: ${environment.apiUrl}`);
+  }
+
+  ngOnInit() {
+    this.progressBarService.progressRef = this.progressBar.ref('myProgress');
   }
 }
