612cc526f441b1607489b28347e81d69aacfc7c4
spartanGhost17 <adambaku5216@gmail.com>
2024-02-26 17:35:37 +0000

    moved password reset in one level up in the file structure

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/auth/reset-password/reset-password.component.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/auth/reset-password/reset-password.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.scss']
 })
