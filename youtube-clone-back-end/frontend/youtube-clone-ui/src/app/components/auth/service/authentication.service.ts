38618695c97fdb7198796bad943096aa7c400658
spartanGhost17 <adambaku5216@gmail.com>
2024-02-27 14:50:35 +0000

    added resetPassword, verifyPasswordLink & and updatePassword to auth service

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/auth/service/authentication.service.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/auth/service/authentication.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
@@ -7,6 +7,9 @@ import { CurrentUserInterface } from '../../../shared/types/currentUser.interfac
 import { HttpResponseInterface } from '../../../shared/types/httpResponse.interface';
 import { LoginFormInterface } from '../types/loginForm.interface';
 import { RegisterFormInterface } from '../types/registerForm.interface';
+import { ResponseMessagesInterface } from '../../../shared/types/responseMessages.interface';
+import { UpdatePasswordFormInterface } from '../types/updatePasswordForm.interface';
+import { VerifyPasswordInterface } from '../types/verifyPassword.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -40,6 +43,58 @@ export class AuthenticationService {
     return this.http.post<HttpResponseInterface<CurrentUserInterface>>(
       this.apiUrl + resources.USER_END_POINTS.REGISTER,
       registerForm
+    );
+  }
+
+  /**
+   * reset password
+   * @param {string} email
+   * @returns {Observable<HttpResponseInterface<ResponseMessagesInterface>>} the reponse message
+   */
+  resetPassword(
+    email: string
+  ): Observable<HttpResponseInterface<ResponseMessagesInterface>> {
+    let params = new HttpParams();
+    params = params.append('email', email.trim());
+    return this.http.get<HttpResponseInterface<ResponseMessagesInterface>>(
+      this.apiUrl + resources.USER_END_POINTS.RESET_PASSWORD,
+      { params: params }
+    );
+  }
+
+  /**
+   * verify password link
+   * @param {VerifyPasswordInterface} verifyPasswordForm
+   * @returns {Observable<HttpResponseInterface<ResponseMessagesInterface>>}
+   */
+  verifyPasswordLink(
+    verifyPasswordForm: VerifyPasswordInterface
+  ): Observable<HttpResponseInterface<ResponseMessagesInterface>> {
+    const baseUrl = this.apiUrl;
+    const url = new URL(this.apiUrl + resources.USER_END_POINTS.VERIFY_PASSWORD_URL, baseUrl);
+    const params = new HttpParams()
+            .append('type', verifyPasswordForm.type)
+            .append('key', verifyPasswordForm.key);
+    console.log(`URL:  ${url}`);
+    return this.http.post<HttpResponseInterface<ResponseMessagesInterface>>(url.toString(), params);
+  }
+
+  /**
+   * The update the password for the user
+   * @param {string} key the UUID key
+   * @param {UpdatePasswordFormInterface} updatePasswordForm
+   * @returns {Observable<HttpResponseInterface<ResponseMessagesInterface>>} the response
+   */
+  updatePassword(
+    key: string,
+    updatePasswordForm: UpdatePasswordFormInterface
+  ): Observable<HttpResponseInterface<ResponseMessagesInterface>> {
+    let params = new HttpParams();
+    params = params.append('key', key);
+    return this.http.post<HttpResponseInterface<ResponseMessagesInterface>>(
+      this.apiUrl + resources.USER_END_POINTS.RESET_PASSWORD,
+      updatePasswordForm,
+      { params }
     );
   }
 }
