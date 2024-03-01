07e48e0fcc897fda0d85bbaf30c6eb376fddd6d6
spartanGhost17 <adambaku5216@gmail.com>
2024-03-01 18:25:52 +0000

    created report component

---
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/report/report/report.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-report',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './report.component.html',
+  styleUrls: ['./report.component.scss']
+})
+export class ReportComponent {
+
+}
