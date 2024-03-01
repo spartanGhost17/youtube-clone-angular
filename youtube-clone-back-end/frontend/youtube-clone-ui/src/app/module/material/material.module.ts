2feff0a46e9870fbff3a34bbfd3f91314db05f59
spartanGhost17 <adambaku5216@gmail.com>
2024-03-01 18:32:51 +0000

    updated material imports

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/module/material/material.module.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/module/material/material.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [],
   exports: [
     MatSnackBarModule,
-    MatRippleModule
+    MatRippleModule,
+    MatFormFieldModule, 
+    MatSelectModule,
+    MatInputModule 
   ]
 })
 export class MaterialModule { }
