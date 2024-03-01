44266a6c7457155ad8d984b8fc22c62d24767bec
spartanGhost17 <adambaku5216@gmail.com>
2024-03-01 18:35:37 +0000

    added permissions on navigation panel

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/pages/dashboard-view/dashboard-view.component.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/pages/dashboard-view/dashboard-view.component.ts
@@ -23,6 +23,7 @@ export class DashboardViewComponent {
     this.section = [
       {'isActive':true, text: 'Dashboard', icon: 'dashboard', provider: 'google', type: 'outlined', color: '#ff4e46', navigateTo: 'dashboard', sidebarType: 'side'},//this.ICON_HOME
       {'isActive':false, text: 'Content', icon: 'slideshow', provider: 'google', type: 'outlined', color: '#ff4e46', navigateTo: 'content', sidebarType: 'side'},//
+      {'isActive':false, text: 'Your permissions', icon: 'key', provider: 'google', type: 'outlined', color: '#ff4e46', navigateTo: 'permissions', sidebarType: 'side'},//
       {'isActive':false, text: 'Analytics', icon: 'insert_chart', provider: 'google', type: 'outlined', color: '#ff4e46', navigateTo: 'dashboard', sidebarType: 'side'},//this.ICON_SUBSCRIPTION
       {'isActive':false, text: 'Comments', icon: 'chat', provider: 'google', type: 'outlined', color: '#ff4e46', navigateTo: 'dashboard', sidebarType: 'side'},
       {'isActive':false, text: 'Subtitles', icon: 'insert_chart', provider: 'google', type: 'outlined', color: '#ff4e46', navigateTo: 'dashboard', sidebarType: 'side'},
