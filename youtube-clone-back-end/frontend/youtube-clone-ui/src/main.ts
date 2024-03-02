b4dcaf4618bd0e829050a9c7e5342d02780edd85
spartanGhost17 <adambaku5216@gmail.com>
2024-03-02 18:25:32 +0000

    registered user state, effects & reducer in main.ts

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/main.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/main.ts
@@ -1,19 +1,22 @@
 import {
   provideHttpClient,
-  withInterceptors,
-  withInterceptorsFromDi,
+  withInterceptorsFromDi
 } from '@angular/common/http';
 import { importProvidersFrom, isDevMode } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { NZ_I18N, uk_UA } from 'ng-zorro-antd/i18n';
 import { NgxFileDropModule } from 'ngx-file-drop';
 import { appRoutes } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
+import { httpInterceptorProviders } from './app/components/auth/interceptors/interceptorProvider/httpInterceptorProvider';
+import * as authEffects from './app/components/auth/store/effects';
 import {
   authFeatureKey,
   authReducer,
@@ -21,12 +24,10 @@ import {
 import { IconsProviderModule } from './app/module/icons-provider.module';
 import { MaterialModule } from './app/module/material/material.module';
 import { NgProgressBarModule } from './app/module/ngProgress-bar/ng-progress.module';
-import { provideEffects } from '@ngrx/effects';
-import * as authEffects from './app/components/auth/store/effects';
-import * as permissionsEffects from './app/shared/store/permission/effects'
-import { provideRouterStore, routerReducer } from '@ngrx/router-store';
-import { httpInterceptorProviders } from './app/components/auth/interceptors/interceptorProvider/httpInterceptorProvider';
+import * as permissionsEffects from './app/shared/store/permission/effects';
+import * as userEffects from './app/shared/store/user/effects';
 import { permissionsFeatureKey, permissionsReducer } from './app/shared/store/permission/reducers';
+import { userFeatureKey, userReducer } from './app/shared/store/user/reducers';
 
 
 bootstrapApplication(AppComponent, {
@@ -49,15 +50,16 @@ bootstrapApplication(AppComponent, {
     }), //register store
     provideState(authFeatureKey, authReducer), //registered here since the auth feature is shared
     provideState(permissionsFeatureKey, permissionsReducer),
-    provideEffects(authEffects, permissionsEffects),
+    provideState(userFeatureKey, userReducer),
+    provideEffects(authEffects, permissionsEffects, userEffects),
     provideRouterStore(), //will helps us manage router navigation in angular way (delete some fields)
     provideStoreDevtools({
-      //maxAge: 25, //max actions
+      maxAge: 25, //max actions
       logOnly: !isDevMode(),
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       trace: false, //If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-    }),
+    }),  
     { provide: NZ_I18N, useValue: uk_UA },
     //provideHttpClient(),
     provideAnimations(),
