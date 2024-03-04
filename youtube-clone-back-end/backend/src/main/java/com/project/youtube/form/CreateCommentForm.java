d6b87fcafafcc27cf99890fa9cd762e409f80f55
spartanGhost17 <adambaku5216@gmail.com>
2024-03-04 14:25:12 +0000

    added a comment creation form

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/form/CreateCommentForm.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/form/CreateCommentForm.java
@@ -1,5 +1,27 @@
 package com.project.youtube.form;
 
-public class CreateCommentForm {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.Nullable;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.sql.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCommentForm {
+    //@NotNull(message = "userId cannot be empty")
+    @Nullable
+    private Long userId;
+    @NotNull(message = "videoId cannot be empty")
+    private Long videoId;
+    @NotEmpty(message = "comment text cannot be empty")
+    private String commentText;
+    @Nullable
+    private Long parentCommentId;
 }
