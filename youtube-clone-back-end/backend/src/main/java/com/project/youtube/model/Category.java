94228e15db32be6afd7bf79085e3835d13f65f4e
spartanGhost17 <adambaku5216@gmail.com>
2024-03-04 21:43:13 +0000

    created a category object

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/model/Category.java
@@ -0,0 +1,24 @@
+package com.project.youtube.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Column;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Data
+@EntityScan
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(NON_NULL)
+public class Category {
+    private Long id;
+    @Column(name = "category_name")
+    private String categoryName;
+}
