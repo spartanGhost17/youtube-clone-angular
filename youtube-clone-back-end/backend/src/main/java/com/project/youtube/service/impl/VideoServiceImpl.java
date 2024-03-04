635d90973703886c0444f17b03713b3012ef985b
spartanGhost17 <adambaku5216@gmail.com>
2024-03-04 14:32:54 +0000

    updated the video service

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/impl/VideoServiceImpl.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/impl/VideoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.project.youtube.service.impl;
 
+import com.project.youtube.dao.impl.VideoDaoImpl;
 import com.project.youtube.dto.VideoDto;
+import com.project.youtube.dtomapper.VideoDTOMapper;
+import com.project.youtube.form.UpdateVideoMetadataForm;
 import com.project.youtube.repository.VideoRepository;
 import com.project.youtube.service.VideoService;
 import lombok.RequiredArgsConstructor;
@@ -14,19 +17,61 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.NoSuchElementException;
 
+import static com.project.youtube.dtomapper.VideoDTOMapper.toVideoDto;
+
 @Service
 @RequiredArgsConstructor
 //@Component
 public class VideoServiceImpl implements VideoService {
 
-    //auto-wire
     @Autowired
     private final S3Service s3Service;
-    //@Autowired
-    //private final VideoRepository videoRepository;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(VideoServiceImpl.class);
-    public VideoDto uploadVideo(MultipartFile multipartFile){
+    private final VideoDaoImpl videoDao;
+    @Override
+    public VideoDto uploadVideo(MultipartFile multipartFile, Long userId) {
+        return VideoDTOMapper.toVideoDto(videoDao.create(multipartFile, userId));
+    }
+
+    /**
+     * update video metadata
+     * @param updateVideoMetadataForm
+     * @return video dto
+     */
+    @Override
+    public VideoDto updateVideoMetadata(UpdateVideoMetadataForm updateVideoMetadataForm) {
+        return mapToVideoDto(videoDao.updateMetadata(updateVideoMetadataForm));
+    }
+
+    @Override
+    public String uploadVideoThumbnail(MultipartFile thumbnailImage, Long videoId) {
+        return null;
+    }
+
+    @Override
+    public VideoDto getVideoMetadataById(Long id) {
+        return mapToVideoDto(videoDao.getVideo(id));
+    }
+
+    @Override
+    public VideoDto updateVideoCategory(Long videoId, Long categoryId) {
+        return null;
+    }
+
+    @Override
+    public VideoDto updateVideoStatus(Long videoId, Long statusId) {
+        return null;
+    }
+
+    /**
+     * map video object to dto
+     * @param video
+     * @return
+     */
+    private VideoDto mapToVideoDto(Video video) {
+        return toVideoDto(video);
+    }
+    /*public VideoDto uploadVideo(MultipartFile multipartFile){
         LOGGER.info("Entering uploadVideo in com.project.youtube.service.impl.S3Service");
         // Upload file to AWS S3
         // Save Video Data to MongoDB DataBase
@@ -40,11 +85,11 @@ public class VideoServiceImpl implements VideoService {
         return new VideoDto(savedVideo.getId(), null, null, null, null, null, null,null, savedVideo.getVideoUrl());
     }
 
-    /**
+    **
      * Update video metadata information in database
      * @param videoDto
      * @return
-     */
+     *
     public VideoDto updateVideoMetadata(VideoDto videoDto) {
         LOGGER.info("Entering updateVideoMetadata");
         LOGGER.info("Update mongoDB video metadata for id : "+videoDto.getId());
@@ -56,9 +101,9 @@ public class VideoServiceImpl implements VideoService {
         // map videoDto fields to Video POJO
         videoToSave.setTitle(videoDto.getTitle());
         videoToSave.setDescription(videoDto.getDescription());
-        videoToSave.setThumbnailURL(videoDto.getThumbnailURL());
+        videoToSave.setThumbnailUrl(videoDto.getThumbnailURL());
         //videoToSave.setTags(videoDto.getTags());
-        videoToSave.setVisibilityStatus(videoDto.getVisibilityStatus());
+        //videoToSave.setVisibilityStatus(videoDto.getVisibilityStatus());
 
         // save video to database
         //videoRepository.save(videoToSave);
@@ -71,7 +116,7 @@ public class VideoServiceImpl implements VideoService {
         LOGGER.info("Entering uploadVideoThumbnail for video id: "+videoId);
         Video videoToSave = getVideoById(videoId);
         String thumbnailUrl = s3Service.uploadFile(multipartFile);
-        videoToSave.setThumbnailURL(thumbnailUrl);
+        videoToSave.setThumbnailUrl(thumbnailUrl);
         //videoRepository.save(videoToSave);
         LOGGER.info("Leaving uploadVideoThumbnail");
         return thumbnailUrl;
@@ -85,6 +130,8 @@ public class VideoServiceImpl implements VideoService {
                 .orElseThrow(() -> {
                     LOGGER.error("Cannot retreive video by id:" + videoId);
                     return new NoSuchElementException("Cannot retreive video by id: " + videoId);
-                });*/
-    }
+                });
+    }*/
+
+
 }
