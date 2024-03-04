5a0a0a859180bc509fd41df4398347c3251eb727
spartanGhost17 <adambaku5216@gmail.com>
2024-03-04 19:21:28 +0000

    updated comment query

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/query/CommentQuery.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/query/CommentQuery.java
@@ -6,6 +6,6 @@ public class CommentQuery {
     public static final String INSERT_SUB_COMMENT_QUERY = "INSERT INTO Comments (user_id, video_id, comment_text, parent_comment_id) VALUES (:userId, :videoId, :commentText, :parentCommentId)";
     public static final String DELETE_COMMENT_QUERY = "DELETE FROM Comments WHERE id = :id";
     public static final String UPDATE_COMMENT_QUERY = "UPDATE Comments SET comment_text = :commentText WHERE id = :id";
-    public static final String SELECT_COMMENT_PAGE_QUERY = "SELECT * FROM Comments WHERE video_id = :videoId AND parent_comment_id IS NULL LIMIT :pageSize OFFSET :offset";
-    public static final String SELECT_SUB_COMMENT_PAGE_QUERY = "SELECT * FROM Comments WHERE video_id = :videoId AND parent_comment_id IS NOT NULL LIMIT :pageSize OFFSET :offset";
+    public static final String SELECT_COMMENT_PAGE_QUERY = "SELECT * FROM Comments WHERE video_id = :videoId AND parent_comment_id IS NULL ORDER BY created_at DESC LIMIT :pageSize OFFSET :offset";
+    public static final String SELECT_SUB_COMMENT_PAGE_QUERY = "SELECT * FROM Comments WHERE video_id = :videoId AND parent_comment_id IS NOT NULL ORDER BY created_at DESC LIMIT :pageSize OFFSET :offset";
 }
