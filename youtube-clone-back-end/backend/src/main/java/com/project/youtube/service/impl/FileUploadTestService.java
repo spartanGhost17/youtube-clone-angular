d82d848f1ac688b19c352d2f6c1133f27fd757fd
spartanGhost17 <adambaku5216@gmail.com>
2024-03-04 14:31:15 +0000

    updated the testUpload service to handle videos

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/impl/FileUploadTestService.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/impl/FileUploadTestService.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -57,6 +59,65 @@ public class FileUploadTestService {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * upload video
+     * @param video the video
+     * @param videoUrl the url
+     */
+    public String saveVideo(MultipartFile video, String videoUrl) {
+        String uploadUrl;
+        Path fileStorageLocation = Paths.get(System.getProperty("user.home")+"/Downloads/videos").toAbsolutePath().normalize();
+        log.info("Got file path {}", fileStorageLocation);
+        if(!Files.exists(fileStorageLocation)) {
+            try {
+                log.info("creating directory it does not exist");
+                Files.createDirectories(fileStorageLocation);
+            } catch (Exception exception) {
+                log.info("random error");
+                log.error(exception.getMessage());
+                throw new APIException("Could not create directories to save user profile images");
+            }
+            log.info("Created image directories");
+        }
+        try {
+            String extension = getVideoFileExtension(video);
+            Path fileName = Paths.get(videoUrl + extension);
+            log.info("Saved video {}",fileName.getFileName().toString());
+            uploadUrl = fileName.getFileName().toString();
+
+            //Files.copy(video.getInputStream(), fileStorageLocation.resolve(fileName.getFileName().toString()));
+            // Save the video to the specified folder
+            File outputFile = new File(fileStorageLocation.toFile(), uploadUrl);
+            FileOutputStream outputStream = new FileOutputStream(outputFile);
+            outputStream.write(video.getBytes());
+            outputStream.close();
+
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        log.info("FIle saved in: {} folder", fileStorageLocation);
+        return uploadUrl;
+    }
+
+    //Files.write(filePath, video.getBytes());
+    //Files.write(video.getInputStream(), fileStorageLocation.resolve(videoUrl + "."+extension));//, StandardCopyOption.REPLACE_EXISTING);
+
+    /**
+     * get the video file extension
+     * @param video the video multipart
+     * @return the extension
+     */
+    public String getVideoFileExtension(MultipartFile video) {
+        // Get the original filename
+        String originalFilename = video.getOriginalFilename();
+        // Get the extension from the original filename
+        String extension = "";
+        if (originalFilename != null && originalFilename.contains(".")) {
+            extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        }
+        return extension;
     }
 
 }
