c6de404159a2f3c228883f94673d1ec9b6590f59
spartanGhost17 <adambaku5216@gmail.com>
2024-03-07 21:56:02 +0000

    created playlist dao

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/dao/PlaylistDao.java
@@ -0,0 +1,21 @@
+package com.project.youtube.dao;
+
+import com.project.youtube.dto.VideoDto;
+import com.project.youtube.form.PlaylistForm;
+import com.project.youtube.form.VideoItemForm;
+import com.project.youtube.model.Playlist;
+
+import java.util.List;
+
+public interface PlaylistDao<T> {
+    void create(PlaylistForm playlistForm);
+    Playlist getByPlaylistId(Long playlistId);
+    List<Playlist> getByUserId(Long userId);
+    void updateVideosPosition(List<VideoItemForm> videosList);
+    Playlist updatePlaylist(PlaylistForm playlistForm);
+    void addVideo(VideoItemForm videoItemForm);
+    List<VideoDto> getVideos(Long playlistId);
+    boolean isFull(Long playlistId);
+    Integer getPlaylistSize(Long playlistId);
+    void delete(Long playlistId, Long userId);
+}
