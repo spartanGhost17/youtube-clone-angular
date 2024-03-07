5b84d8d2119c9fef98256513c0370a4a87663bcb
spartanGhost17 <adambaku5216@gmail.com>
2024-03-07 22:02:26 +0000

    added position and status to video dto

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/dto/VideoDto.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/dto/VideoDto.java
@@ -1,11 +1,13 @@
 package com.project.youtube.dto;
 
 
+import com.project.youtube.model.Status;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import java.sql.Time;
 import java.sql.Timestamp;
 
@@ -13,8 +15,10 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @Getter
 @Setter
+//@JsonInclude(NON_NULL)
 public class VideoDto {
     private Long id;
+    private Long position;
     private Long userId;
     private String title;
     private String description;
@@ -32,4 +36,5 @@ public class VideoDto {
     private boolean reported;
     //extra fields
     private Long likeCount;
+    private Status status;
 }
