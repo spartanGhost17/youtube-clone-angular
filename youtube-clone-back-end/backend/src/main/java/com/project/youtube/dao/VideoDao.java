11db5e5c1c9103b068605c878a28e9c3ad122d3b
spartanGhost17 <adambaku5216@gmail.com>
2024-03-07 22:01:05 +0000

    updated video dao

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/dao/VideoDao.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/dao/VideoDao.java
@@ -1,10 +1,13 @@
 package com.project.youtube.dao;
 
 import com.project.youtube.form.UpdateVideoMetadataForm;
+import com.project.youtube.model.Category;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface VideoDao<T> {
     T create(MultipartFile video, Long userId);
     T getVideo(Long id);
     T updateMetadata(UpdateVideoMetadataForm videoMetadataForm);
+    Category getVideoCategory(Long videoId);
+    void delete(Long videoId, Long userId);
 }
