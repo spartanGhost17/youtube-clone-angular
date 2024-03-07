6478bec88cf8849718524fd4fbbe3f3c4219c36e
spartanGhost17 <adambaku5216@gmail.com>
2024-03-07 22:08:20 +0000

    created playlist status

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/model/PlaylistStatus.java
@@ -0,0 +1,26 @@
+package com.project.youtube.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Column;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Data
+@EntityScan
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(NON_NULL)
+public class PlaylistStatus {
+    private Long id;
+    @Column(name = "playlist_id")
+    private Long playlistId;
+    @Column(name = "status_id")
+    private Long statusId;
+}
