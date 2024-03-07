127897bc6a96141edf9414c725eee872d1e2a351
spartanGhost17 <adambaku5216@gmail.com>
2024-03-07 22:03:17 +0000

    updated video service

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/VideoService.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/VideoService.java
@@ -2,7 +2,8 @@ package com.project.youtube.service;
 
 import com.project.youtube.dto.VideoDto;
 import com.project.youtube.form.UpdateVideoMetadataForm;
-import com.project.youtube.model.Video;
+import com.project.youtube.model.Category;
+import com.project.youtube.model.Status;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface VideoService {
@@ -10,7 +11,7 @@ public interface VideoService {
     VideoDto updateVideoMetadata(UpdateVideoMetadataForm updateVideoMetadataForm);
     String uploadVideoThumbnail(MultipartFile thumbnailImage, Long videoId);
     VideoDto getVideoMetadataById(Long id);
-    VideoDto updateVideoCategory(Long videoId, Long categoryId);
-    VideoDto updateVideoStatus(Long videoId, Long statusId);
-
+    Category getVideoCategory(Long videoId);
+    Status updateVideoStatus(Long videoId, Long statusId);
+    void delete(Long videoId, Long userId);
 }
