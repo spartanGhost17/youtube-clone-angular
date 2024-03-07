9b169928962b0b831bde7979a09452a2393174c5
spartanGhost17 <adambaku5216@gmail.com>
2024-03-07 21:45:11 +0000

    Added @component annotation to commentDTOMapper

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/dtomapper/commentDTOMapper.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/dtomapper/commentDTOMapper.java
@@ -3,7 +3,9 @@ package com.project.youtube.dtomapper;
 import com.project.youtube.dto.CommentDto;
 import com.project.youtube.model.Comment;
 import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
 
+@Component
 public class commentDTOMapper {
 
     public static CommentDto toCommentDto(Comment comment) {
