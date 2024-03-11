b13242799ff8f51b8b1c81fc916046ea37fb5ba4
spartanGhost17 <adambaku5216@gmail.com>
2024-03-11 14:28:21 +0000

    added subscription controller

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/controller/SubscriptionController.java
@@ -0,0 +1,12 @@
+package com.project.youtube.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.project.youtube.constants.ApplicationConstants.API_VERSION;
+
+@RestController
+@RequestMapping(API_VERSION + "subscription/")
+public class SubscriptionController {
+
+}
