82e1bea4204435eb05a8d9d363c57f53302c4815
spartanGhost17 <adambaku5216@gmail.com>
2024-03-11 14:25:15 +0000

    added create playlist and get user playlist in playlist service

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/services/playlist/playlist.service.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/services/playlist/playlist.service.ts
@@ -1,27 +1,41 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { PlaylistForm } from '../../types/playlistForm.interface';
 import { Observable } from 'rxjs';
-import { Playlist } from '../../types/playlist.interface';
-import end_points  from "../../../../resources/end-points.json"
-
+import { HttpResponseInterface } from '../../types/httpResponse.interface';
+import { PlaylistInterface } from '../../types/playlist.interface';
+import { buildURL } from '../../utils/sharedUtils';
+import resources from '../../../../resources/end-points.json' 
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaylistService {
+  apiUrl: string = environment.apiUrl;
+  PLAYLIST = resources.PLAYLIST_END_POINTS;
+  constructor(private http: HttpClient) { }
 
-  constructor(private httpClient: HttpClient) { }
+  /**
+   * create a playlist
+   * @param {PlaylistForm} playlistForm the playlist creation form 
+   * @returns response
+   */
+  createPlaylist(playlistForm: PlaylistForm): Observable<HttpResponseInterface<PlaylistInterface>> {
+    const url: string = buildURL(this.apiUrl, this.PLAYLIST.CREATE);
+    return this.http.post<HttpResponseInterface<PlaylistInterface>>(url, playlistForm);
+  }
 
-  httpOptions = {
-    headers : new HttpHeaders({
-      'Access-Control-Allow-Credentials' : 'true',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, PUT, OPTIONS',
-      'Authorization': ''
-    })
-  };
-
-  createPlaylist(playlist: Playlist) : Observable<Playlist> {
-    return this.httpClient.put<Playlist>(end_points.PLAYLIST_END_POINTS.CREATE_PLAYLIST, playlist, this.httpOptions)
+  /**
+   * get the user's playlists
+   * @param {number} userId the user id
+   * @returns {HttpResponseInterface<PlaylistInterface>} the user's playlists
+   */
+  getUserPlaylists(userId: number): Observable<HttpResponseInterface<PlaylistInterface>> {
+    const url: string = buildURL(this.apiUrl, this.PLAYLIST.GET_BY_USER);
+    const params = new HttpParams()
+                  .append('userId', userId);
+    
+    return this.http.get<HttpResponseInterface<PlaylistInterface>>(url, {params});
   }
 }
