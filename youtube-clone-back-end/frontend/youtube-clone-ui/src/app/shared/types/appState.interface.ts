89a804af07c4348f73e067b74e82e1bf62a6996d
spartanGhost17 <adambaku5216@gmail.com>
2024-03-11 14:28:03 +0000

    moved these files

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/types/appState.interface.ts
+++
@@ -1,5 +0,0 @@
-import { UserInterface } from "./user.interface";
-
-export interface AppStateInterface {
-    user: UserInterface;
-}
--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/types/currentUserState.interface.ts
+++
@@ -1,9 +0,0 @@
-import { CurrentUserInterface } from "./currentUser.interface";
-import { ResponseMessagesInterface } from "./responseMessages.interface";
-
-export interface CurrentUserStateInterface {
-    isLoading: boolean;
-    currentUser: CurrentUserInterface | null | undefined,
-    validationMessages: ResponseMessagesInterface | null,
-    validationErrors: ResponseMessagesInterface | null
-}
