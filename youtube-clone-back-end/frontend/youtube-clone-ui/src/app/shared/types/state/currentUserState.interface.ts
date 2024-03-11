a3d699af12ddbcd8722ee89c1996bd9aeb06a995
spartanGhost17 <adambaku5216@gmail.com>
2024-03-11 14:38:43 +0000

    moved current user state

---
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/types/state/currentUserState.interface.ts
@@ -0,0 +1,9 @@
+import { CurrentUserInterface } from "../currentUser.interface";
+import { ResponseMessagesInterface } from "../responseMessages.interface";
+
+export interface CurrentUserStateInterface {
+    isLoading: boolean;
+    currentUser: CurrentUserInterface | null | undefined,
+    validationMessages: ResponseMessagesInterface | null,
+    validationErrors: ResponseMessagesInterface | null
+}
