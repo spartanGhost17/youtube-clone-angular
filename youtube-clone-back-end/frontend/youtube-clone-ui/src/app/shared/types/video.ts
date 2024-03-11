4408b1f0254b1e950edf9e443a1ed3fc0267e1bc
spartanGhost17 <adambaku5216@gmail.com>
2024-03-11 14:34:14 +0000

    updated video interface

---
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/types/video.ts
@@ -0,0 +1,45 @@
+import { Time } from "@angular/common";
+import { Tag } from "../../models/tag";
+import { Status } from "./status.interface";
+import { UserInterface } from "./user.interface";
+
+export interface Video {
+    /*id: string;
+    title: string;
+    description?: string;
+    likeCount?: number;
+    createdAt?: Date; 
+    tags?: Tag[];
+    status: Status;
+    thumbnailURL?: string;
+    videoURL?: string;
+    upload_date?: string;
+    duration?: number;
+    views?: number;
+    user?: UserInterface;
+    position?: number; 
+    commentEnabled?: boolean*/
+    id: number;
+    position?: number;
+    userId?: number;
+    title: string;
+    description?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    duration?: Time;
+    stopAt?: Time;
+    totalBytes?: number;
+    stopAtBytes?: number;
+    views?: number;
+    commentEnabled?: boolean;
+    thumbnailUrl?: string;
+    videoUrl?: string;
+    location?: string;
+    reported?: boolean;
+    //extra fields
+    likeCount?: number;
+    status?: Status;
+    gifUrl?: string;
+    tags?: Tag[];
+    user?: UserInterface
+}
