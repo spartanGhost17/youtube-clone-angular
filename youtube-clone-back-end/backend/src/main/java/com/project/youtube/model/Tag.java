f2fce4f1c49b172fa32b888337b8f3190fa43d13
spartanGhost17 <adambaku5216@gmail.com>
2024-03-11 14:30:11 +0000

    added tag model

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/model/Tag.java
@@ -0,0 +1,24 @@
+package com.project.youtube.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Column;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Data
+@EntityScan
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(NON_NULL)
+public class Tag {
+    private Long id;
+    @Column(name = "tag_name")
+    private String tagName;
+}
