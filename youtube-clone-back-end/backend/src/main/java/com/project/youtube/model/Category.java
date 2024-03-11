fb471b660dccb573c573acbbc4d2119dcdc72203
spartanGhost17 <adambaku5216@gmail.com>
2024-03-11 14:05:18 +0000

    update category POJO

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/model/Category.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/model/Category.java
@@ -7,8 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import javax.persistence.Column;
-
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @Data
@@ -19,6 +17,5 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 @JsonInclude(NON_NULL)
 public class Category {
     private Long id;
-    @Column(name = "category_name")
-    private String categoryName;
+    private String name;
 }
