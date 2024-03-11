57e5d3bfd0a9b5ac6aae259bd2de9542646459be
spartanGhost17 <adambaku5216@gmail.com>
2024-03-11 14:12:04 +0000

    updated save video test function

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/impl/FileUploadTestService.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/service/impl/FileUploadTestService.java
@@ -112,37 +112,38 @@ public class FileUploadTestService {
      */
     public String saveVideo(MultipartFile video, String videoUrl) {
         String uploadUrl;
-        Path fileStorageLocation = Paths.get(System.getProperty("user.home")+"/Downloads/videos").toAbsolutePath().normalize();
+        Path fileStorageLocation = Paths.get(System.getProperty("user.home") + VIDEOS_DEFAULT_FOLDER).toAbsolutePath().normalize();
         log.info("Got file path {}", fileStorageLocation);
         if(!Files.exists(fileStorageLocation)) {
             try {
                 log.info("creating directory it does not exist");
                 Files.createDirectories(fileStorageLocation);
             } catch (Exception exception) {
-                log.info("random error");
                 log.error(exception.getMessage());
-                throw new APIException("Could not create directories to save user profile images");
+                throw new APIException("Could not create directories to save video");
             }
-            log.info("Created image directories");
+            log.info("Created video directories");
         }
         try {
             String extension = getVideoFileExtension(video);
             Path fileName = Paths.get(videoUrl + extension);
-            log.info("Saved video {}",fileName.getFileName().toString());
+            //log.info("Saved video {}",fileName.getFileName().toString());
             uploadUrl = fileName.getFileName().toString();
 
             //Files.copy(video.getInputStream(), fileStorageLocation.resolve(fileName.getFileName().toString()));
             // Save the video to the specified folder
             File outputFile = new File(fileStorageLocation.toFile(), uploadUrl);
+            log.info("Video full path {}", outputFile.getAbsolutePath());
+
             FileOutputStream outputStream = new FileOutputStream(outputFile);
             outputStream.write(video.getBytes());
             outputStream.close();
 
+            //log.info("FIle saved in: {} folder", fileStorageLocation);
+            return uploadUrl;
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
-        log.info("FIle saved in: {} folder", fileStorageLocation);
-        return uploadUrl;
     }
 
     //Files.write(filePath, video.getBytes());
