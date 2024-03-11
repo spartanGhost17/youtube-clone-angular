a5f80fb5c199343b614a9cea1c69f976f6a84219
spartanGhost17 <adambaku5216@gmail.com>
2024-03-11 14:30:29 +0000

    created video thumbnail

---
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/model/VideoThumbnail.java
@@ -0,0 +1,27 @@
+package com.project.youtube.model;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Column;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Data
+@EntityScan
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(NON_NULL)
+public class VideoThumbnail {
+    private Long id;
+    @Column(name = "video_id")
+    private Long videoId;
+    @Column(name = "thumbnail_url")
+    private String thumbnailUrl;
+}
