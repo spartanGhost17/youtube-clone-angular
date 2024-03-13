dc7bbf7b8ee52d532f5c7e268bee2342c46c32e9
spartanGhost17 <adambaku5216@gmail.com>
2024-03-13 10:32:00 +0000

    whitelisted video thumbnail resource

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/auth/interceptors/authInterceptor.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/auth/interceptors/authInterceptor.ts
@@ -27,7 +27,8 @@ const PUBLIC_URLS: string[] = [
     API_VERSION + 'user/verify/password',
     API_VERSION + 'user/verify/account',
     API_VERSION + 'user/refresh/token',
-    API_VERSION + 'user/image'
+    API_VERSION + 'user/image',
+    API_VERSION + 'video/thumbnail'
 ];
 
 
