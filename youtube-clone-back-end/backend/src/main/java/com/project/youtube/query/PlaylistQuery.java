3b34abd002f6f1bee353536a241d14242b0275e6
spartanGhost17 <adambaku5216@gmail.com>
2024-03-14 18:29:55 +0000

    update playlist queries

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/query/PlaylistQuery.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/query/PlaylistQuery.java
@@ -11,4 +11,5 @@ public class PlaylistQuery {
     public static final String UPDATE_PLAYLIST_VIDEO_POSITION_QUERY = "UPDATE PlaylistVideos SET position = :position WHERE video_id = :videoId AND playlist_id = :playlistId";
     public static final String DELETE_PLAYLIST_QUERY = "DELETE FROM Playlists WHERE id = :playlistId AND user_id = :userId";
     public static final String SELECT_PLAYLIST_BY_VIDEO_ID_QUERY = "SELECT p.* FROM PlaylistVideos AS pv INNER JOIN Playlists AS p ON p.id = pv.playlist_id WHERE pv.video_id = :videoId AND p.user_id = :userId AND LOWER(p.name) NOT IN ('history', 'watch later', 'likes')";
+    public static final String DELETE_PLAYLIST_VIDEO_BY_VIDEO_ID = "DELETE FROM PlaylistVideos WHERE video_id = :videoId AND playlist_id = :playlistId";
 }
