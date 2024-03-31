ac581e67808c17f3dcfa105b974d35733fae43a5
spartanGhost17 <adambaku5216@gmail.com>
2024-03-31 17:20:42 +0100

    updated video description and removed comments

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/video-description/video-description.component.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/video-description/video-description.component.ts
@@ -1,19 +1,20 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { NewlineToBrPipe } from '../../pipes/newline-to-br/newline-to-br.pipe';
+import { DatePipe, NgIf } from '@angular/common';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { LinkifyPipe } from '../../pipes/linkify/linkify.pipe';
+import { NewlineToBrPipe } from '../../pipes/newline-to-br/newline-to-br.pipe';
+import { Video } from '../../shared/types/video';
 
 @Component({
     selector: 'app-video-description',
     templateUrl: './video-description.component.html',
     styleUrls: ['./video-description.component.scss'],
     standalone: true,
-    imports: [LinkifyPipe, NewlineToBrPipe]
+    imports: [LinkifyPipe, NewlineToBrPipe, NgIf, DatePipe]
 })
 export class VideoDescriptionComponent {
   @ViewChild('content') content : ElementRef<any>;
-  text: string = `Video description is here so here is a link https://www.youtube.com/ \n 
-  Here is a second link: http://www.youtube.com/watch \n 
-  Some text linking to History:  http://localhost:4200/home/feed/history  \n`;
+  @Input() metadata: Video;
+
   MAX_CONTENT_HEIGHT : string = '400px';
   MIN_CONTENT_HEIGHT : string = '50px';
   SHOW_MORE: string = '...more';
@@ -24,24 +25,20 @@ export class VideoDescriptionComponent {
 
 
   showMore() {
-    console.log(`show more: ${this.isShowMore}`);
     this.isShowMore = !this.isShowMore;
-    console.log(`show more after: ${this.isShowMore}`);
     if(this.isShowMore) {
       this.content.nativeElement.style.height = this.MAX_CONTENT_HEIGHT;
       this.SHOW_MORE  = 'Show less';
-      this.isShowLess = false;//!this.isShowLess;
+      this.isShowLess = false;
     }
   }
 
   showLess() {
-    console.log(`show less: ${this.isShowLess}`);
     this.isShowLess = !this.isShowLess;
-    console.log(`show less after: ${this.isShowLess}`);
     if(this.isShowLess) {
       this.content.nativeElement.style.height = this.MIN_CONTENT_HEIGHT;
       this.SHOW_MORE  = '...more';
-      this.isShowMore = false;//!this.isShowMore;
+      this.isShowMore = false;
     }
   }
 }
