70e3668ff5816febbfb523acb167073d9ce94d7c
spartanGhost17 <adambaku5216@gmail.com>
2024-03-31 17:12:55 +0100

    removed unused imports

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/feed/playlist/playlist-mini/playlist-mini.component.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/feed/playlist/playlist-mini/playlist-mini.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PlaylistInterface } from '../../../../shared/types/playlist.interface';
 import { StandardDropdownComponent } from '../../../dropdown/standard-dropdown/standard-dropdown.component';
 
