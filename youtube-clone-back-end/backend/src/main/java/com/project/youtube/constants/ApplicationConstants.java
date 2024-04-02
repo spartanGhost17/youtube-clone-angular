ad6b3250fbe8f919418f760d4601ae224904f141
spartanGhost17 <adambaku5216@gmail.com>
2024-04-02 20:48:04 +0100

    updated constants

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/constants/ApplicationConstants.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/constants/ApplicationConstants.java
@@ -8,7 +8,7 @@ public class ApplicationConstants {
     public static final String USER_VIDEO_MANAGEMENT_SERVICE = "Video management service";
     public static final String JWT_AUTHORITIES_KEY = "authorities";
     public static final String JWT_ROLE_KEY = "role";
-    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 30L * 60 * 1000;//30 minutes TODO: SET EXPIRATION TO 5 DAYS FOR TESTING, REMOVE THIS LATER
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 2L * 24 * 60 * 60 * 1000;//30L * 60 * 1000;//30 minutes TODO: SET EXPIRATION TO 5 DAYS FOR TESTING, REMOVE THIS LATER
     public static final long REFRESH_TOKEN_EXPIRATION_TIME =  5L * 24 * 60 * 60 * 1000;//5 days //TODO:SET TO 30 SECONDS FOR TESTING 30_000;
     public static final String PUBLIC_KEY_PATH = "src/main/resources/rsa/rsa.public";
     public static final String PRIVATE_KEY_PATH = "src/main/resources/rsa/rsa.private";
@@ -16,6 +16,7 @@ public class ApplicationConstants {
     public static final String DEFAULT_VIDEO_TITLE = "Video title";
     public static final int PLAYLIST_MAX_SIZE = 4000;
     public static final String DEFAULT_VIDEO_VISIBILITY = "DRAFT";
+    public static final String DEFAULT_PLAYLIST_VISIBILITY = "PUBLIC";
     public static final String[] DEFAULT_PLAYLISTS = {"Likes", "Watch Later", "History"};
     public static final String VIDEOS_DEFAULT_FOLDER = "/Downloads/videos";
     public static final String GIFS_DEFAULT_FOLDER = "/Downloads/gifs";
