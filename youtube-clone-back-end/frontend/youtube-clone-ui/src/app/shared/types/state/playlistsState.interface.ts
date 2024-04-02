0e5d3da635fc644d0467aa805ac04af5692eb123
spartanGhost17 <adambaku5216@gmail.com>
2024-04-02 21:21:38 +0100

    updated endpoints and commented fields in playlistState

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/types/state/playlistsState.interface.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/types/state/playlistsState.interface.ts
@@ -4,6 +4,9 @@ import { ResponseMessagesInterface } from "../responseMessages.interface";
 export interface PlaylistsStateInterface {
     isLoading: boolean;
     playlists: PlaylistInterface[];
+    /*history: PlaylistInterface | null;
+    watchLater: PlaylistInterface | null;
+    liked: PlaylistInterface | null;*/
     ValidationMessages: ResponseMessagesInterface | null;
     ValidationErrors: ResponseMessagesInterface | null;
 }
