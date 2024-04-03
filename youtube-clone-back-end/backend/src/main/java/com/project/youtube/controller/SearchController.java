3d5679b786d13a67c3a0a77cc52375f1b13dcd58
spartanGhost17 <adambaku5216@gmail.com>
2024-04-03 19:30:28 +0100

    updated search controller

--- youtube-clone-back-end/backend/src/main/java/com/project/youtube/controller/SearchController.java
+++ youtube-clone-back-end/backend/src/main/java/com/project/youtube/controller/SearchController.java
@@ -1,2 +1,13 @@
-package com.project.youtube.controller;public class SearchController {
+package com.project.youtube.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.project.youtube.constants.ApplicationConstants.API_VERSION;
+
+@RequestMapping(API_VERSION + "search/")
+@RestController
+@RequiredArgsConstructor
+public class SearchController {
 }
