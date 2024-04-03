cd65d61def54d9cea0ce7c2e3e03d6d370fa1979
spartanGhost17 <adambaku5216@gmail.com>
2024-04-03 19:44:05 +0100

    updated playlist interface

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/types/playlist.interface.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/types/playlist.interface.ts
@@ -1,9 +1,11 @@
+import { Status } from './status.interface';
 import { Video } from './video';
 export interface PlaylistInterface {
     id?:number;
     name: string;
+    thumbnailUrl?: string;
     description?: string;
     videos?: Video[];
-    visibilityStatus: string;
+    visibilityStatus: Status;
     size?: number;
 }
