67760c6fa61883e7be4e99ff821c3767cf22b483
spartanGhost17 <adambaku5216@gmail.com>
2024-04-07 22:17:23 +0100

    updated subscriptions section

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/pages/home/home.component.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/pages/home/home.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { FrameComponent } from '../../components/frame/frame.component';
 import { Icons } from '../../models/icons';
-import { selectCurrentUser } from '../../shared/store/user/reducers';
+import { selectCurrentUser, selectSubscriptions } from '../../shared/store/user/reducers';
 import { ComponentUpdatesService } from '../../shared/services/app-updates/component-updates.service';
+import { userActions } from '../../shared/store/user/actions';
 
 @Component({
     selector: 'app-home',
@@ -29,20 +30,18 @@ export class HomeComponent {
 
   ngOnInit(): void {
     this.componentUpdatesService.sideBarTypeUpdate('side');
-    
+    this.populateSidePanel('', []);
     this.store.select(selectCurrentUser).subscribe({
       next: (data) => {
         if(data) {
           this.username = data.username;
-          this.populateSidePanel(this.username);
+          this.getSubscriptions();
         }
       }
     });
-    
-    this.populateSidePanel('');
   }
 
-  populateSidePanel(username: string) {
+  populateSidePanel(username: string, subcribedChannels: any[]) {
     this.section1 = [
       {'isActive':true, text: 'Home', icon: 'home', provider: 'google', type: 'fill', color: 'white', navigateTo: 'explore', sidebarType: 'side'},//this.ICON_HOME
       {'isActive':false, text: 'Shorts', icon: this.ICON_SHORTS, provider: 'default', type: 'outlined', color: 'white', navigateTo: '', sidebarType: 'side'},//
@@ -50,40 +49,43 @@ export class HomeComponent {
     ];
   
     this.section2 = [
-      {'isActive':false, text: 'Your channel', icon: 'account_box', provider: 'google', type: 'outlined', color: 'white', navigateTo: `home/@${username}`, sidebarType: 'side', isChannel: true},//this.ICON_LIBRARY_VIDEO
+      {'isActive':false, username: `${username}`, text: 'Your channel', icon: 'account_box', provider: 'google', type: 'outlined', color: 'white', navigateTo: `home/@`, sidebarType: 'side', isChannel: true},//this.ICON_LIBRARY_VIDEO
       {'isActive':false, text: 'History', icon: 'history', provider: 'google', type: 'outlined', color: 'white', navigateTo: 'feed/history', sidebarType: 'side'},//this.ICON_HISTORY
       {'isActive':false, text: 'Your videos', icon: 'slideshow', provider: 'google', type: 'outlined', color: 'white', navigateTo: 'studio/dashboard', sidebarType: 'side', newTab: true},//this.ICON_LIBRARY
       {'isActive':false, text: 'Watch Later', icon: 'schedule', provider: 'google', type: 'outlined', color: 'white', navigateTo: 'playlist', sidebarType: 'side', playlistName: 'WL'},//this.ICON_WATCH_LATER
       {'isActive':false, text: 'Liked Videos', icon: 'thumb_up', provider: 'google', type: 'outlined', color: 'white', navigateTo: 'playlist', sidebarType: 'side', playlistName: 'LL'},//this.ICON_LIKE playlistName: 'PLlFiFXELFq7g4nk3ydFeZLdvqacmEfPGG'
       {'isActive':false, text: 'Show more', icon: 'expand_more', provider: 'google', type: 'outlined', color: 'white', navigateTo: 'feed/library', sidebarType: 'side'},
     ];
-  
-    this.subcribed_channels = [
-      {'isActive':false, text: 'One Piece', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'DBZ Hollo', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Channel 3', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Channel4777777777777777777777777 7777777777777777777777777777777', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: '', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Channel 5', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Channel 6', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Channel 7', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Channel 8', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Channel 9', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Channel 10', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Channel4 about ben solo  777777777777777777777777 7777777777777777777777777777777', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: '', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Cartoon Network kids', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Double Champ ent.', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'WWE', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'arte', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'History portrait', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-      {'isActive':false, text: 'Legends of History', channel_user_icon: this.ICON_USER, icon_right: this.ICON_RADIO_SIGNAL, navigateTo: 'home/@', sidebarType: 'side', isChannel: true},
-
-    ];
-  
-
-    this.sideMenuOptions = [this.section1, this.section2, this.subcribed_channels];
+    this.subcribed_channels = subcribedChannels;
+    this.sideMenuOptions = this.subcribed_channels? [this.section1, this.section2, this.subcribed_channels] : [this.section1, this.section2];
   }
 
+  /**
+   * get user subscriptions
+   */
   getSubscriptions() {
-
+    this.store.select(selectSubscriptions).subscribe({
+      next: (subscriptions) => {
+        if(!subscriptions) {
+          this.store.dispatch(userActions.getSubscriptions());
+        } else if (subscriptions.length > 0) {
+          let subsList = subscriptions.map(u => {
+            return {
+              isActive: false, 
+              text: u.channelName,
+              username: u.username, 
+              channel_user_icon: u.profilePicture, 
+              icon_right: this.ICON_RADIO_SIGNAL, 
+              navigateTo: 'home/@', 
+              sidebarType: 'side', 
+              isChannel: true}
+          });
+          console.log("subscriptions ", subsList);
+          this.populateSidePanel(this.username, subsList);  
+        } else {
+          this.populateSidePanel(this.username, []);
+        }
+      }
+    });
   }
 }
