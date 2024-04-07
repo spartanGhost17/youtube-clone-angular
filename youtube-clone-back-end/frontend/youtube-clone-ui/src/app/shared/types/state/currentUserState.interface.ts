63f8e209c7311beede8e3f41034e1795eac4b19c
spartanGhost17 <adambaku5216@gmail.com>
2024-04-07 22:20:14 +0100

    updated CurrentUserState

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/types/state/currentUserState.interface.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/shared/types/state/currentUserState.interface.ts
@@ -1,9 +1,11 @@
 import { CurrentUserInterface } from "../currentUser.interface";
 import { ResponseMessagesInterface } from "../responseMessages.interface";
+import { UserInterface } from "../user.interface";
 
 export interface CurrentUserStateInterface {
     isLoading: boolean;
     currentUser: CurrentUserInterface | null | undefined,
+    subscriptions: UserInterface[] | undefined,
     validationMessages: ResponseMessagesInterface | null,
     validationErrors: ResponseMessagesInterface | null
 }
