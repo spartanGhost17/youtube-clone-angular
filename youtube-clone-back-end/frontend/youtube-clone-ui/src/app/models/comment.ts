78ef8835b0c13cbade1b6bd9ac1aa70614fba3cf
spartanGhost17 <adambaku5216@gmail.com>
2024-04-07 22:16:32 +0100

    updated comment model in angular

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/models/comment.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/models/comment.ts
@@ -1,18 +1,53 @@
 import { SubComment } from "./subComment";
 
 export interface Comment {
-    id?: number;
-    userId?: number,
-    username?: string;
-    imageUrl?: string,
-    commentText?: string,
-    postTime?: string,
-    replyCount?: number,
-    likeCount?: number,
-    dislikeCount?: number,
-    subComments?: SubComment[],
-    like?: boolean,
-    dislike?: boolean,
-    showReply?: boolean,
-    showReplies?: boolean
+    //id?: number;
+    //userId?: number,
+    //username?: string;
+    //imageUrl?: string,
+    //commentText?: string,
+    //postTime?: string,
+    //replyCount?: number,
+    //likeCount?: number,
+
+    id: number;
+    userId: number;
+    videoId: number;
+    commentText: string;
+    createdAt: Date;
+    lastUpdated: Date;
+    parentCommentId: number | undefined;
+    //extra fields
+    likeCount: number;
+    replyCount: number;
+    imageUrl: string;
+    username: string;
+
+    dislikeCount?: number;
+    subComments: SubComment[];
+    like?: boolean;
+    dislike?: boolean;
+    showReply?: boolean;
+    showReplies?: boolean;
+    reported?: boolean;
+    pageSize?: number;
+    offset?: number;
+    to?: string;
+
+    /**
+     * 
+    id: number;
+    userId: number;
+    videoId: number;
+    commentText: string;
+    createdAt: Timestamp;
+    lastUpdated: Timestamp;
+    parentCommentId: number;
+    reported: number;
+    //extra fields
+    likeCount: number;
+    replyCount: number;
+    imageUrl: string;
+    username: string;
+     */
 }
