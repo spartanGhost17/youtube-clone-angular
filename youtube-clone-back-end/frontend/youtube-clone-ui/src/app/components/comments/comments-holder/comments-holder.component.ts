e00235f2f469b133410ec31948850dc3ebc63f0d
spartanGhost17 <adambaku5216@gmail.com>
2024-04-07 22:11:30 +0100

    add create comment service function to comment holder

--- youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/comments/comments-holder/comments-holder.component.ts
+++ youtube-clone-back-end/frontend/youtube-clone-ui/src/app/components/comments/comments-holder/comments-holder.component.ts
@@ -1,11 +1,16 @@
 import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Comment } from '../../../models/comment';
 import { selectCurrentUser } from '../../../shared/store/user/reducers';
 import { CurrentUserInterface } from '../../../shared/types/currentUser.interface';
 import { CommentComponent } from '../comment/comment.component';
+import { CommentService } from '../../../shared/services/comment/comment.service';
+import { CommentRequestForm } from '../../../shared/types/commentReqForm.interface';
+import { CommentForm } from '../../../shared/types/commentForm.interface';
+import { SubComment } from '../../../models/subComment';
+import { HttpResponseInterface } from '../../../shared/types/httpResponse.interface';
 @Component({
     selector: 'app-comments-holder',
     templateUrl: './comments-holder.component.html',
@@ -16,13 +21,11 @@ import { CommentComponent } from '../comment/comment.component';
 export class CommentsHolderComponent implements OnInit {
   loading: boolean;
   @Input() comments: Comment[];
-  @Input() user: CurrentUserInterface;/*: any = {
-    userId: 'timbaWolf',
-    iconURL: '../../../assets/batman_red_glow.jpg'
-  };*/
+  @Input() user: CurrentUserInterface;
+  @Input() videoId: number;
   commentInput: string;
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private commentService: CommentService) {}
 
   ngOnInit(): void {
     this.loading = true;
@@ -44,13 +47,16 @@ export class CommentsHolderComponent implements OnInit {
       id: commentsCount, //ID will come from the db auto incrementing
       username: this.user.username,
       commentText: this.commentInput,
-      imageUrl: this.user.profilePicture,
+      imageUrl: this.user.profilePicture!,
       userId: this.user.id,
       likeCount: 0,
       dislikeCount: 0,
-      postTime: '2 hours',
-      replyCount: 0,   
-      subComments: []
+      createdAt: new Date(),
+      lastUpdated: new Date(),
+      replyCount: 0,
+      subComments: [],
+      videoId: this.videoId,
+      parentCommentId: 0
     }
     this.commentInput = '';
     this.comments = [...this.comments, comment];
@@ -68,12 +74,41 @@ export class CommentsHolderComponent implements OnInit {
     // Find the index of the originalComment in the comments array
     const index = this.comments.indexOf(updatedComment)//(originalComment);
     console.log("index", index);
+    
+    const ogIndex = this.comments.indexOf(originalComment)
+    console.log("ogIndex", ogIndex);
+    
     // Update the comments array with the updatedComment at the found index
-    if (index !== -1) {
-      this.comments[index] = updatedComment;
-    }
+    const commentForm: CommentForm = {
+      userId: this.user.id,
+      videoId: this.videoId,
+      commentText: updatedComment.commentText,//remove and replace 
+      parentCommentId: updatedComment.id
+    };
+    
+    this.commentService.create(commentForm).subscribe({
+      next: (response: HttpResponseInterface<Comment>) => {
+        if (index !== -1) {
+          const comment: Comment = response.data.comment;
+          const subComment: SubComment = {
+            createdAt: comment.createdAt,
+            lastUpdated: comment.lastUpdated,
+            text: comment.commentText,
+            to: updatedComment.username,
+            username: comment.username,
+            imageUrl: comment.imageUrl,
+            parentId: comment.parentCommentId!,
+            likeCount: comment.likeCount
+          }; //response.comment;
 
-    console.log("whole comment list after ", this.comments);
+          this.comments[index].subComments = [...this.comments[index].subComments, subComment];//updatedComment;
+        }
+
+        console.log("whole comment list after ", this.comments);
+      }
+    })
+    
+
+    
   }
-
 }
